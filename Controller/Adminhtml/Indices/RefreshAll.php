4787b1335847af6d8bbb1a23d625814994231d65
tjons <tyler@swiftotter.com>
2017-02-06 14:37:57 +0000

    Added composer.json

---
+++ Controller/Adminhtml/Indices/RefreshAll.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @by SwiftOtter, Inc., 2/9/17
+ * @website https://swiftotter.com
+ **/
+
+namespace TylerSchade\IndexManagement\Controller\Adminhtml\Indices;
+
+use Magento\Backend\App\Action;
+
+class RefreshAll extends Action
+{
+    public function execute()
+    {
+        $indices = $this->indexCollection->getItems();
+
+        try {
+            array_walk($indices, function (Indexer $index) {
+                $index->reindexAll();
+            });
+
+            $indexCount = count($indices);
+
+            $this->getMessageManager()->addSuccessMessage("$indexCount indexes were refreshed.");
+        } catch (\Exception $e) {
+            $this->getMessageManager()->addErrorMessage('An error occurred. Please try again later.');
+        }
+
+        $redirect = $this->resultRedirectFactory->create();
+
+        return $redirect->setPath('*/');
+    }
+}
